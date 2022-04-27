d94eb238369d1ddd3356d447f34f7551772f7807
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2022-04-26 21:07:54 -0700

    - added function to get coin flow from the use case and update the state

--- app/src/main/java/com/nfragiskatos/cryptocurrencybrowswer/presentation/coin_list/CoinListViewModel.kt
+++ app/src/main/java/com/nfragiskatos/cryptocurrencybrowswer/presentation/coin_list/CoinListViewModel.kt
@@ -3,8 +3,11 @@ package com.nfragiskatos.cryptocurrencybrowswer.presentation.coin_list
 import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.nfragiskatos.cryptocurrencybrowswer.common.Resource
 import com.nfragiskatos.cryptocurrencybrowswer.domain.use_case.get_coins.GetCoinsUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -15,4 +18,26 @@ class CoinListViewModel @Inject constructor(
     private val _state = mutableStateOf<CoinListState>(CoinListState())
     val state: State<CoinListState> = _state
 
+    init {
+        getCoins()
+    }
+
+    private fun getCoins() {
+        viewModelScope.launch {
+            getCoinsUseCase().collect { result ->
+                when (result) {
+                    is Resource.Error -> {
+                        _state.value =
+                            CoinListState(error = result.message ?: "An unexpected error occurred")
+                    }
+                    is Resource.Loading -> {
+                        _state.value = CoinListState(isLoading = result.isLoading)
+                    }
+                    is Resource.Success -> {
+                        _state.value = CoinListState(coins = result.data ?: emptyList())
+                    }
+                }
+            }
+        }
+    }
 }
