8f618777974b8e383343d8141a652c71b288f628
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2022-04-26 20:54:21 -0700

    - setup dependency injection - added AppModule.kt to define providers for the CoinPaprikaApi and the CoinRepository interfaces - Created necessary CoinApplication.kt annotated with HiltAndroidApp - updated AndroidManifest.xml

---
+++ app/src/main/java/com/nfragiskatos/cryptocurrencybrowswer/CoinApplication.kt
@@ -0,0 +1,7 @@
+package com.nfragiskatos.cryptocurrencybrowswer
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class CoinApplication : Application()
